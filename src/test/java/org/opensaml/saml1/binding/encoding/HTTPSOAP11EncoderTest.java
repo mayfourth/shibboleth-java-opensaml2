2fb9c22d42bbeb972ff580001180451e2880e7ff
Brent Putman <putmanb@georgetown.edu>
2013-01-12 01:13:35 +0000

    JOST-199: SAML SOAP encoders should use the supplied outbound SOAP Envelope from the message context, if it exists

--- src/test/java/org/opensaml/saml1/binding/encoding/HTTPSOAP11EncoderTest.java
+++ src/test/java/org/opensaml/saml1/binding/encoding/HTTPSOAP11EncoderTest.java
@@ -25,7 +25,12 @@ import org.opensaml.common.binding.BasicSAMLMessageContext;
 import org.opensaml.saml1.core.Request;
 import org.opensaml.saml2.metadata.AssertionConsumerService;
 import org.opensaml.saml2.metadata.Endpoint;
+import org.opensaml.ws.soap.common.SOAPObjectBuilder;
+import org.opensaml.ws.soap.soap11.Envelope;
+import org.opensaml.ws.soap.soap11.Header;
 import org.opensaml.ws.transport.http.HttpServletResponseAdapter;
+import org.opensaml.ws.wsaddressing.Action;
+import org.opensaml.ws.wsaddressing.WSAddressingObjectBuilder;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 /**
@@ -64,5 +69,54 @@ public class HTTPSOAP11EncoderTest extends BaseTestCase {
         assertEquals("Unexpected cache controls", "no-cache, no-store", response.getHeader("Cache-control"));
         assertEquals("http://www.oasis-open.org/committees/security", response.getHeader("SOAPAction"));
         assertEquals(-2097500418, response.getContentAsString().hashCode());
+    }
+    
+    /** Tests encoding a simple SAML message using a pre-existing SOAP Envelope. */
+    @SuppressWarnings("unchecked")
+    public void testExistingEnvelope() throws Exception {
+        SAMLObjectBuilder<Request> requestBuilder = (SAMLObjectBuilder<Request>) builderFactory
+                .getBuilder(Request.DEFAULT_ELEMENT_NAME);
+        Request request = requestBuilder.buildObject();
+        request.setID("foo");
+        request.setIssueInstant(new DateTime(0));
+        request.setVersion(SAMLVersion.VERSION_11);
+
+        SAMLObjectBuilder<Endpoint> endpointBuilder = (SAMLObjectBuilder<Endpoint>) builderFactory
+                .getBuilder(AssertionConsumerService.DEFAULT_ELEMENT_NAME);
+        Endpoint samlEndpoint = endpointBuilder.buildObject();
+        samlEndpoint.setLocation("http://example.org");
+        samlEndpoint.setResponseLocation("http://example.org/response");
+        
+        SOAPObjectBuilder<Envelope> envelopeBuilder = (SOAPObjectBuilder<Envelope>) builderFactory
+                .getBuilder(Envelope.DEFAULT_ELEMENT_NAME);
+        Envelope envelope = envelopeBuilder.buildObject();
+        
+        SOAPObjectBuilder<Header> headerBuilder = (SOAPObjectBuilder<Header>) builderFactory
+                .getBuilder(Header.DEFAULT_ELEMENT_NAME);
+        Header header = headerBuilder.buildObject();
+        envelope.setHeader(header);
+        
+        WSAddressingObjectBuilder<Action> actionBuilder = (WSAddressingObjectBuilder<Action>) builderFactory
+                .getBuilder(Action.ELEMENT_NAME);
+        Action action = actionBuilder.buildObject();
+        action.setValue("urn:test:action");
+        header.getUnknownXMLObjects().add(action);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        BasicSAMLMessageContext messageContext = new BasicSAMLMessageContext();
+        messageContext.setOutboundMessageTransport(new HttpServletResponseAdapter(response, false));
+        messageContext.setPeerEntityEndpoint(samlEndpoint);
+        messageContext.setOutboundSAMLMessage(request);
+        messageContext.setOutboundMessage(envelope);
+        messageContext.setRelayState("relay");
+        
+        HTTPSOAP11Encoder encoder = new HTTPSOAP11Encoder();
+        encoder.encode(messageContext);
+
+        assertEquals("Unexpected content type", "text/xml", response.getContentType());
+        assertEquals("Unexpected character encoding", response.getCharacterEncoding(), "UTF-8");
+        assertEquals("Unexpected cache controls", "no-cache, no-store", response.getHeader("Cache-control"));
+        assertEquals("http://www.oasis-open.org/committees/security", response.getHeader("SOAPAction"));
+        assertEquals(-191617364, response.getContentAsString().hashCode());
     }
 }
--- src/test/java/org/opensaml/saml2/binding/encoding/HTTPSOAP11EncoderTest.java
+++ src/test/java/org/opensaml/saml2/binding/encoding/HTTPSOAP11EncoderTest.java
@@ -27,7 +27,12 @@ import org.opensaml.saml2.core.Status;
 import org.opensaml.saml2.core.StatusCode;
 import org.opensaml.saml2.metadata.AssertionConsumerService;
 import org.opensaml.saml2.metadata.Endpoint;
+import org.opensaml.ws.soap.common.SOAPObjectBuilder;
+import org.opensaml.ws.soap.soap11.Envelope;
+import org.opensaml.ws.soap.soap11.Header;
 import org.opensaml.ws.transport.http.HttpServletResponseAdapter;
+import org.opensaml.ws.wsaddressing.Action;
+import org.opensaml.ws.wsaddressing.WSAddressingObjectBuilder;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 /**
@@ -83,5 +88,71 @@ public class HTTPSOAP11EncoderTest extends BaseTestCase {
         assertEquals("Unexpected cache controls", "no-cache, no-store", response.getHeader("Cache-control"));
         assertEquals("http://www.oasis-open.org/committees/security", response.getHeader("SOAPAction"));
         assertEquals(2034125980, response.getContentAsString().hashCode());
+    }
+    
+    /**
+     * Tests encoding a SAML message to an servlet response using a pre-existing SOAP Envelope.
+     * 
+     * @throws Exception
+     */
+    @SuppressWarnings("unchecked")
+    public void testExistingEnvelope() throws Exception {
+        SAMLObjectBuilder<StatusCode> statusCodeBuilder = (SAMLObjectBuilder<StatusCode>) builderFactory
+                .getBuilder(StatusCode.DEFAULT_ELEMENT_NAME);
+        StatusCode statusCode = statusCodeBuilder.buildObject();
+        statusCode.setValue(StatusCode.SUCCESS_URI);
+
+        SAMLObjectBuilder<Status> statusBuilder = (SAMLObjectBuilder<Status>) builderFactory
+                .getBuilder(Status.DEFAULT_ELEMENT_NAME);
+        Status responseStatus = statusBuilder.buildObject();
+        responseStatus.setStatusCode(statusCode);
+
+        SAMLObjectBuilder<Response> responseBuilder = (SAMLObjectBuilder<Response>) builderFactory
+                .getBuilder(Response.DEFAULT_ELEMENT_NAME);
+        Response samlMessage = responseBuilder.buildObject();
+        samlMessage.setID("foo");
+        samlMessage.setVersion(SAMLVersion.VERSION_20);
+        samlMessage.setIssueInstant(new DateTime(0));
+        samlMessage.setStatus(responseStatus);
+
+        SAMLObjectBuilder<Endpoint> endpointBuilder = (SAMLObjectBuilder<Endpoint>) builderFactory
+                .getBuilder(AssertionConsumerService.DEFAULT_ELEMENT_NAME);
+        Endpoint samlEndpoint = endpointBuilder.buildObject();
+        samlEndpoint.setLocation("http://example.org");
+        samlEndpoint.setResponseLocation("http://example.org/response");
+        
+        SOAPObjectBuilder<Envelope> envelopeBuilder = (SOAPObjectBuilder<Envelope>) builderFactory
+                .getBuilder(Envelope.DEFAULT_ELEMENT_NAME);
+        Envelope envelope = envelopeBuilder.buildObject();
+        
+        SOAPObjectBuilder<Header> headerBuilder = (SOAPObjectBuilder<Header>) builderFactory
+                .getBuilder(Header.DEFAULT_ELEMENT_NAME);
+        Header header = headerBuilder.buildObject();
+        envelope.setHeader(header);
+        
+        WSAddressingObjectBuilder<Action> actionBuilder = (WSAddressingObjectBuilder<Action>) builderFactory
+                .getBuilder(Action.ELEMENT_NAME);
+        Action action = actionBuilder.buildObject();
+        action.setValue("urn:test:action");
+        header.getUnknownXMLObjects().add(action);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        HttpServletResponseAdapter outTransport = new HttpServletResponseAdapter(response, false);
+        
+        BasicSAMLMessageContext messageContext = new BasicSAMLMessageContext();
+        messageContext.setOutboundMessageTransport(outTransport);
+        messageContext.setPeerEntityEndpoint(samlEndpoint);
+        messageContext.setOutboundSAMLMessage(samlMessage);
+        messageContext.setOutboundMessage(envelope);
+        messageContext.setRelayState("relay");
+        
+        HTTPSOAP11Encoder encoder = new HTTPSOAP11Encoder();
+        encoder.encode(messageContext);
+
+        assertEquals("Unexpected content type", "text/xml", response.getContentType());
+        assertEquals("Unexpected character encoding", response.getCharacterEncoding(), "UTF-8");
+        assertEquals("Unexpected cache controls", "no-cache, no-store", response.getHeader("Cache-control"));
+        assertEquals("http://www.oasis-open.org/committees/security", response.getHeader("SOAPAction"));
+        assertEquals(1218694958, response.getContentAsString().hashCode());
     }
 }
