2774642f61b5f8ae80eb51a7bbf4f2c6e32b30fc
Brent Putman <putmanb@georgetown.edu>
2013-03-22 19:14:48 +0000

    JOST-208: FileBackedHTTPMetadataProvider constructor doesn't behave correctly vis-a-vis fail-fast setting if the backing file path has problems

--- src/test/java/org/opensaml/saml2/metadata/provider/FileBackedHTTPMetadataProviderTest.java
+++ src/test/java/org/opensaml/saml2/metadata/provider/FileBackedHTTPMetadataProviderTest.java
@@ -20,7 +20,6 @@ package org.opensaml.saml2.metadata.provider;
 import java.io.File;
 
 import org.opensaml.common.BaseTestCase;
-import org.opensaml.saml2.metadata.EntitiesDescriptor;
 
 /**
  * Test case for {@link FileBackedHTTPMetadataProvider}.
@@ -42,10 +41,8 @@ public class FileBackedHTTPMetadataProviderTest extends BaseTestCase {
         mdUrl = "https://svn.shibboleth.net/java-opensaml2"
                 + "/branches/REL_2/src/test/resources/data/org/opensaml/saml2/metadata/ukfederation-metadata.xml";
         badMDURL = "http://www.google.com/";
-        backupFilePath = System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") + "metadata.xml";
-        metadataProvider = new FileBackedHTTPMetadataProvider(mdUrl, 1000 * 5, backupFilePath);
-        metadataProvider.setParserPool(parser);
-        metadataProvider.initialize();
+        backupFilePath = System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") 
+                + "filebacked-http-metadata.xml";
     }
 
     /** {@inheritDoc} */
@@ -58,29 +55,121 @@ public class FileBackedHTTPMetadataProviderTest extends BaseTestCase {
      * Tests the {@link HTTPMetadataProvider#getMetadata()} method.
      */
     public void testGetMetadata() throws MetadataProviderException {
-        EntitiesDescriptor descriptor = (EntitiesDescriptor) metadataProvider.getMetadata();
-        assertNotNull("Retrieved metadata was null", descriptor);
+        metadataProvider = new FileBackedHTTPMetadataProvider(mdUrl, 1000 * 5, backupFilePath);
+        metadataProvider.setParserPool(parser);
+        metadataProvider.initialize();
+        
+        assertNotNull("Retrieved metadata was null", metadataProvider.getMetadata());
 
         File backupFile = new File(backupFilePath);
         assertTrue("Backup file was not created", backupFile.exists());
         assertTrue("Backup file contains no data", backupFile.length() > 0);
-
-        // Test pulling it from the backup file
-        FileBackedHTTPMetadataProvider badProvider = new FileBackedHTTPMetadataProvider(badMDURL, 1000 * 5,
-                backupFilePath);
-        badProvider.setParserPool(parser);
+    }
+    
+    /**
+     * Test fail-fast = true with known bad metadata URL.
+     */
+    public void testFailFastBadURL() throws MetadataProviderException {
+        metadataProvider = new FileBackedHTTPMetadataProvider(badMDURL, 1000 * 5, backupFilePath);
         
-        try{
-            badProvider.initialize();
+        metadataProvider.setFailFastInitialization(true);
+        metadataProvider.setParserPool(parser);
+        
+        try {
+            metadataProvider.initialize();
             fail("metadata provider claims to have parsed known invalid data");
-        }catch(MetadataProviderException e){
-            //expected this
+        } catch (MetadataProviderException e) {
+            //expected, do nothing
+        }
+    }
+    
+    /**
+     * Test fail-fast = false with known bad metadata URL.
+     */
+    public void testNoFailFastBadURL() throws MetadataProviderException {
+        metadataProvider = new FileBackedHTTPMetadataProvider(badMDURL, 1000 * 5, backupFilePath);
+        
+        metadataProvider.setFailFastInitialization(false);
+        metadataProvider.setParserPool(parser);
+        
+        try {
+            metadataProvider.initialize();
+        } catch (MetadataProviderException e) {
+            fail("Provider failed init with fail-fast=false");
+        }
+    }
+    
+    /**
+     *  Test fail-fast = true and bad backup file
+     */
+    public void testFailFastBadBackupFile() {
+        try {
+            // Use a known existing directory as backup file path, which is an invalid argument.
+            metadataProvider = new FileBackedHTTPMetadataProvider(mdUrl, 1000 * 5, System.getProperty("java.io.tmpdir"));
+        } catch (MetadataProviderException e) {
+            fail("Provider failed bad backup file in constructor");
+            
+        }
+        metadataProvider.setFailFastInitialization(true);
+        metadataProvider.setParserPool(parser);
+        
+        try {
+            metadataProvider.initialize();
+            fail("Provider passed init with bad backup file, fail-fast=true");
+        } catch (MetadataProviderException e) {
+            // expected do nothing
+        }
+    }
+    
+    /**
+     *  Test case of fail-fast = false and bad backup file
+     * @throws MetadataProviderException 
+     */
+    public void testNoFailFastBadBackupFile() throws MetadataProviderException {
+        try {
+            // Use a known existing directory as backup file path, which is an invalid argument.
+            metadataProvider = new FileBackedHTTPMetadataProvider(mdUrl, 1000 * 5, System.getProperty("java.io.tmpdir"));
+        } catch (MetadataProviderException e) {
+            fail("Provider failed bad backup file in constructor");
+            
+        }
+        metadataProvider.setFailFastInitialization(false);
+        metadataProvider.setParserPool(parser);
+        
+        try {
+            metadataProvider.initialize();
+        } catch (MetadataProviderException e) {
+            fail("Provider failed init with bad backup file, fail-fast=false");
         }
         
-        badProvider = new FileBackedHTTPMetadataProvider(badMDURL, 1000 * 5,
+        assertNotNull("Metadata retrieved from backing file was null", metadataProvider.getMetadata());
+    }
+    
+    /**
+     * Tests use of backup file on simulated restart.
+     * 
+     * @throws MetadataProviderException
+     */
+    public void testBackupFileOnRestart() throws MetadataProviderException {
+        // Do a setup here to get a good backup file
+        metadataProvider = new FileBackedHTTPMetadataProvider(mdUrl, 1000 * 5, backupFilePath);
+        metadataProvider.setParserPool(parser);
+        metadataProvider.initialize();
+        
+        assertNotNull("Retrieved metadata was null", metadataProvider.getMetadata());
+
+        File backupFile = new File(backupFilePath);
+        assertTrue("Backup file was not created", backupFile.exists());
+        assertTrue("Backup file contains no data", backupFile.length() > 0);
+        
+        // Now do a new provider to simulate a restart (have to set fail-fast=false).
+        // Verify that can use the data from backing file.
+        FileBackedHTTPMetadataProvider badProvider = new FileBackedHTTPMetadataProvider(badMDURL, 1000 * 5,
                 backupFilePath);
         badProvider.setParserPool(parser);
         badProvider.setFailFastInitialization(false);
         badProvider.initialize();
+        
+        assertNotNull("Metadata retrieved from backing file was null", metadataProvider.getMetadata());
     }
 }
